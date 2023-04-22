9dfc627c2ac98691dd56a769afca64b687514a6d
amanverma685 <av36232@gmail.com>
2023-04-22 12:59:22 +0530

    register as student

--- majorproject/src/Screens/Registration.js
+++ majorproject/src/Screens/Registration.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import '../Screens/MyStyle.css'
 function Registration() {
   
-  const [userType,setUserType]=useState("");
+  const [userType,setUserType]=useState(false);
+  useEffect(() => {
+    
+  }, [userType])
   
+
   const [professor, setProfessor] = useState({
+    name: "",
+    email: "",
+    contact_no: "",
+    gender: "",
+    dob: "",
+  });
+
+  const [student, setStudent] = useState({
     name: "",
     email: "",
     contact_no: "",
@@ -17,7 +29,8 @@ function Registration() {
         // console.log(e);
         setProfessor({ ...professor, [e.target.name]: e.target.value });
       };
-      const submitHandler =async(e) => {
+
+      const submitHandlerProfessor =async(e) => {
         e.preventDefault();
         console.log(professor);
         setProfessor({
@@ -30,12 +43,147 @@ function Registration() {
           qualifictaion:""
         })
     }
+    const submitHandlerStudent =async(e) => {
+      e.preventDefault();
+      console.log(professor);
+      setProfessor({
+        name:"",
+        email:"",
+        contact_no:"",
+        gender:"",
+        dob:"",
+        subject:"",
+        qualifictaion:""
+      })
+  }
+
+  const registerUser=()=>{
+    setUserType(!userType);
+  }
+  
+  
+
   return (
-    <div>
+    <>
+    {
+      (userType===false)?(
+      <div>
+        <div className="container">
+        <div className='flex flex-row justify-between'>
+        <div className="title"> Professor Registration</div>
+        <div className='button_gradient'>
+          <button className='m-3' onClick={registerUser} >Register As Student</button>
+        </div>
+        </div>
+        
+        <div className="content">
+          <form className="Form " onSubmit={submitHandlerProfessor}>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Full Name</span>
+                <input
+                  type="text"
+                  placeholder="Enter your full name"
+                  id="name"
+                  name="name"
+                  required
+                  value={professor.name}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  id="email"
+                  name="email"
+                  required
+                  value={professor.email}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input
+                  type="text"
+                  placeholder="Enter your number"
+                  id="contact_no"
+                  name="contact_no"
+                  required
+                  value={professor.contact_no}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Gender</span>
+                <input
+                  type="text"
+                  placeholder="Enter your gender"
+                  id="gender"
+                  name="gender"
+                  required
+                  value={professor.gender}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">DOB</span>
+                <input
+                  type="date"
+                  placeholder="Enter your age"
+                  id="dob"
+                  name="dob"
+                  required
+                  value={professor.dob}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Subject</span>
+                <input
+                  type="text"
+                  placeholder="Enter your subject"
+                  id="subject"
+                  name="subject"
+                  required
+                  value={professor.subject}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Qualification</span>
+                <input
+                  type="text"
+                  placeholder="Enter your full name"
+                  id="qualifictaion"
+                  name="qualifictaion"
+                  required
+                  value={professor.qualifictaion}
+                  onChange={onChange}
+                />
+              </div>
+              </div>
+            <div className="button">
+              <input type="submit" value="Register" />
+            </div>
+            
+          </form>
+        </div>
+      </div>
+    </div>
+    ):(
+      <div>
       <div className="container">
-      <div className="title"> Professor Registration</div>
+      <div className='flex flex-row justify-between'>
+      <div className="title"> Student Registration</div>
+      <div className='button_gradient'>
+        <button  className='m-3'  onClick={registerUser} >Register As Professor</button>
+      </div>
+      </div>
+      
       <div className="content">
-        <form className="Form " onSubmit={submitHandler}>
+        <form className="Form " onSubmit={submitHandlerStudent}>
           <div className="user-details">
             <div className="input-box">
               <span className="details">Full Name</span>
@@ -131,6 +279,9 @@ function Registration() {
     </div>
   </div>
   
+    )
+    }
+    </>
   )
 }
 
